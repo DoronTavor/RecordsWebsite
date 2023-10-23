d7a713cd3071554de6bba727ef8514181ca01be0
DoronTavor <134997382+DoronTavor@users.noreply.github.com>
2023-10-23 09:55:58 +0300

    Update DiscogsReader.js

--- src/components/Readers/DiscogsReader.js
+++ src/components/Readers/DiscogsReader.js
@@ -61,52 +61,52 @@ function allVinyls(keys){
             return {};
         });
 }
-// function asked(key){
-//     return fetch(`https://api.discogs.com/releases/${bigInt(key)}`, {
-//         method: 'GET',
-//         headers: {
-//             'Authorization': authorizationHeader,
-//         }
-//     })
-//         .then((res) => {
-//             if (!res.ok) {
-//                 throw new Error(`HTTP error! Status: ${res.status}`);
-//             }
-//             return res.json();
-//         })
-//         .then((data) => MusicAlbum(data))
-//         .catch((error) => {
-//             console.error("Error fetching data:", error);
-//             return {};
-//         });
-// }
 function asked(key){
-    let keys=[key];
-    const fetchPromises = keys.map(key => {
-        return fetch(`https://api.discogs.com/releases/${bigInt(key)}`, {
-            method: "GET",
-            headers: {
-                'Authorization': authorizationHeader,
+    return fetch(`https://api.discogs.com/releases/${bigInt(key)}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': authorizationHeader,
+        }
+    })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
             }
+            return res.json();
         })
-            .then((res) => res.json())
-            .then((data) => MusicAlbum(data));
-    });
-
-    return Promise.all(fetchPromises)
-        .then((results) => {
-            // 'results' will be an array of MusicAlbum objects for each key
-            const resultObject = {};
-            keys.forEach((key, index) => {
-                resultObject[key] = results[index];
-            });
-            return resultObject;
-        })
+        .then((data) => MusicAlbum(data))
         .catch((error) => {
             console.error("Error fetching data:", error);
             return {};
         });
 }
+// function asked(key){
+//     let keys=[key];
+//     const fetchPromises = keys.map(key => {
+//         return fetch(`https://api.discogs.com/releases/${bigInt(key)}`, {
+//             method: "GET",
+//             headers: {
+//                 'Authorization': authorizationHeader,
+//             }
+//         })
+//             .then((res) => res.json())
+//             .then((data) => MusicAlbum(data));
+//     });
+
+//     return Promise.all(fetchPromises)
+//         .then((results) => {
+//             // 'results' will be an array of MusicAlbum objects for each key
+//             const resultObject = {};
+//             keys.forEach((key, index) => {
+//                 resultObject[key] = results[index];
+//             });
+//             return resultObject;
+//         })
+//         .catch((error) => {
+//             console.error("Error fetching data:", error);
+//             return {};
+//         });
+// }
 
 
 
