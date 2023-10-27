8814563a4e4e96e174ea311581515eebe5e8b5a3
DoronTavor <134997382+DoronTavor@users.noreply.github.com>
2023-10-27 13:06:27 +0300

    Update Title.js

--- src/components/Boxes/Title.js
+++ src/components/Boxes/Title.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import TitleDesign from "styles/TitleDesign.css"
 function title(props){
     return (
-        <div>
-            <h1> {props.name}</h1>
-            <h2> {props.artist}</h2>
-            <h2> {props.format}</h2>
-            <h3>            {props.year}</h3>
+        <div >
+            <h1 className={"centeredName"}> Name:{props.name}</h1>
+            <h2 className={"centeredArtist"}> Artist:{props.artist}</h2>
+            <h2 className={"centeredFormat"}> Format: {props.format}</h2>
+            <h3 className={"year"}>Year: {props.year}</h3>
+            <h2 className={"centeredArtist"}> Label:{props.label}</h2>
+            <h2 className={"centeredCountry"}> Country:{props.country} </h2>
         </div>
     );
 }
