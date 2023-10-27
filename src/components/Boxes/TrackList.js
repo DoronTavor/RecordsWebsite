db8d9a18344e7debd99f9bff6166baeaedc4ae46
DoronTavor <134997382+DoronTavor@users.noreply.github.com>
2023-10-27 13:06:41 +0300

    Update TrackList.js

--- src/components/Boxes/TrackList.js
+++ src/components/Boxes/TrackList.js
@@ -3,12 +3,14 @@ import CustomList from "../cards/CustomList.css";
 import SongCard from "../cards/SongCard";
 function TrackList(props){
     console.log(props.TrackList);
+    let trackList=props.TrackList;
+
     return (
         <ul className="custom-list">
-            {props.TrackList.map((item, index) => (
-                <li key={index} className="custom-list-item" data-custom-marker={item.position.substring(0, 2)}>
+            {trackList.map((item, index) => (
+                <li key={index} className="custom-list-item" data-custom-marker={item.position.substring(0, 2)+"  "}>
                     <div>
-                        <SongCard name={props.name} duration={props.duration}></SongCard>
+                        <SongCard name={item.title.split('=')[0]} duration={item.duration}></SongCard>
                     </div>
                 </li>
             ))}
